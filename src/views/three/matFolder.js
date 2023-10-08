d29320aa657ac5d5d80d0c197c43a5ae3bd095d7
new-wang <1569566735@qq.com>
2023-10-08 17:13:08 +0800

    *：分组

--- src/views/three/matFolder.js
+++ src/views/three/matFolder.js
@@ -1,9 +1,10 @@
 import dat from "dat.gui";
 
 // 创建一个GUI对象
-const matFolder = new dat.GUI();
+const gui = new dat.GUI();
+const matFolder = gui.addFolder('pbr');
 // 修改gui界面默认的style属性
-matFolder.domElement.style.right = '0px'
-matFolder.domElement.style.width = '500px'
+gui.domElement.style.right = '0px'
+gui.domElement.style.width = '500px'
 
  export default matFolder;
